50d6a95984a1c89ee36b94c4bae8038d6bc20b79
Michelle Villagran <mrvillagran16@gmail.com>
2023-06-07 18:35:55 -0600

    feat:add suggested functionality iterations

---
+++ main.js
@@ -0,0 +1,13 @@
+// A function that creates the objects that store each players’ informations - properties should include: id (ex: 'one'), token (ex: '⭐️'), wins (ex: 0)
+// A function called increaseWins - increases the count of a player’s wins (should work for either player)
+// A function that keeps track of the data for the game board
+// A function that keeps track of which player’s turn it currently is
+// A function that checks the game board data for win conditions
+// A function that detects when a game is a draw (no one has won)
+// A function that resets the game board’s data to begin a new game
+
+
+// Your DOM related JavaScript will include:
+// Query selectors
+// Function(s) to display the game board and user data
+// Event listeners
