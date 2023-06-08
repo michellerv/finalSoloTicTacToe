864c264cfb9740cce0d63a32f73c22baa042953b
Michelle Villagran <mrvillagran16@gmail.com>
2023-06-07 18:54:29 -0600

    feat:add function that creates player variables. create fire and water player variable

--- main.js
+++ main.js
@@ -1,4 +1,26 @@
+// Your DOM related JavaScript will include:
+// Query selectors
+// Function(s) to display the game board and user data
+// Event listeners
+
+//functions
+function createPlayer(name, id, token, wins) {
+    return {
+        name: name,
+        id: id,
+        token: token,
+        wins: wins ?? 0
+    }
+}
+
+var firePlayer = createPlayer('fire', 1, 'fireEmoji', 0);
+var waterPlayer = createPlayer('water', 2, 'waterEmoji', 0)
+console.log(firePlayer)
+console.log(waterPlayer)
+
 // A function that creates the objects that store each players’ informations - properties should include: id (ex: 'one'), token (ex: '⭐️'), wins (ex: 0)
+
+
 // A function called increaseWins - increases the count of a player’s wins (should work for either player)
 // A function that keeps track of the data for the game board
 // A function that keeps track of which player’s turn it currently is
@@ -7,7 +29,4 @@
 // A function that resets the game board’s data to begin a new game
 
 
-// Your DOM related JavaScript will include:
-// Query selectors
-// Function(s) to display the game board and user data
-// Event listeners
+
